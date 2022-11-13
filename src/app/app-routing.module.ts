20cc6cdf1980125291741f4e13e0574fa06a6d4d
Sagar, Bhuiya <sagar.bhuiya@optum.com>
2022-11-13 05:59:20 +0530

    app-routing.module imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+
+
 const routes: Routes = [];
 
 @NgModule({
